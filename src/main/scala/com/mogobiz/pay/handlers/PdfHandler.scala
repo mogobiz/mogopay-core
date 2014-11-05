04d2438411b8a97412fa57426830472f581e9a92
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2014-11-05 16:25:01 +0100

    Remove comment

--- src/main/scala/com/mogobiz/pay/handlers/PdfHandler.scala
+++ src/main/scala/com/mogobiz/pay/handlers/PdfHandler.scala
@@ -3,9 +3,7 @@ package com.mogobiz.pay.handlers
 import com.mogobiz.html2pdf.Html2Pdf
 
 
-/**
- * Created by hayssams on 29/10/14.
- */
 class PdfHandler {
   def convertToPdf(page:String, input : String) = Html2Pdf.convert(page,input)
 }
+
