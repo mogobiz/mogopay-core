a73bc3286d2f1e752b0b0b417707dd913006e738
Samy Dindane <samy@dindane.com>
2014-11-27 10:58:01 +0100

    Update hideStringExceptLastN

--- src/test/scala/com/mogobiz/pay/html2pdf/GlobalUtilSpec.scala
+++ src/test/scala/com/mogobiz/pay/html2pdf/GlobalUtilSpec.scala
@@ -3,8 +3,6 @@ package com.mogobiz.pay.html2pdf
 import org.specs2.mutable.Specification
 import com.mogobiz.utils.GlobalUtil._
 
-import scala.util.{Failure, Success}
-
 class GlobalUtilSpec extends Specification {
   "caseClassToMap" should {
     "make a Map out of a case class" in {
@@ -23,11 +21,10 @@ class GlobalUtilSpec extends Specification {
 
   "hideStringExceptLastN" should {
     "hide the string except the last N characters" in {
-      hideStringExceptLastN("", Integer.MAX_VALUE) must_== Success("")
       hideStringExceptLastN("abc", 0) must_== "***"
       hideStringExceptLastN("abc", 1) must_== "**c"
       hideStringExceptLastN("abc", 2) must_== "*bc"
-      hideStringExceptLastN("abc", 3) must_== "abc"
+      hideStringExceptLastN("abc", 3) must_== "aber"
     }
   }
 }
