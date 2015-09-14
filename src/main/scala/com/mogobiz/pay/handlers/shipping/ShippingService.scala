f040c1bd8b2d4357a18e45b3039ad11338834f60
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2015-09-14 16:09:22 +0200

    Handle shipping correctly when no dimension is given

--- src/main/scala/com/mogobiz/pay/handlers/shipping/ShippingService.scala
+++ src/main/scala/com/mogobiz/pay/handlers/shipping/ShippingService.scala
@@ -16,21 +16,27 @@ case class ShippingPrice(shipmentId: String, rateId: String, provider: String, s
 trait ShippingService {
   def calculatePrice(shippingAddress: ShippingAddress, cart: Cart): Seq[ShippingPrice]
 
-  def isManageShipmentId(shippingPrice: ShippingPrice) : Boolean
+  def isManageShipmentId(shippingPrice: ShippingPrice): Boolean
 
-  def confirmShipmentId(shippingPrice: ShippingPrice) : Long
+  def confirmShipmentId(shippingPrice: ShippingPrice): Long
 
   //def confirmPrice()
 
-  def extractShippingContent(cart: Cart) : List[Shipping] = {
+  def extractShippingContent(cart: Cart): List[Shipping] = {
     (for {
       cartItem <- cart.cartItems
-      shipping  <- cartItem.shipping
-    } yield shipping).toList
+      shipping <- cartItem.shipping
+    } yield shipping).flatMap { shipping =>
+      if (shipping.height == 0 || shipping.width == 0 || shipping.weight == 0 || shipping.weightUnit == null || shipping.weightUnit.isEmpty
+      || shipping.linearUnit == null || shipping.linearUnit.isEmpty)
+        None
+      else
+        Some(shipping)
+    } toList
   }
 
-  def createShippingPrice(shipmentId: String, rateId: String, provider: String, service: String, rateType: String, price: Long, currencyCode: String) : ShippingPrice = {
-    var rate : Option[Rate] = rateHandler.findByCurrencyCode(currencyCode)
+  def createShippingPrice(shipmentId: String, rateId: String, provider: String, service: String, rateType: String, price: Long, currencyCode: String): ShippingPrice = {
+    var rate: Option[Rate] = rateHandler.findByCurrencyCode(currencyCode)
     ShippingPrice(shipmentId, rateId, provider, service, rateType, price, currencyCode, if (rate.isDefined) rate.get.currencyFractionDigits else 2)
   }
 }
@@ -46,9 +52,11 @@ object ShippingService {
     }
   }
 
-  def confirmShippingPrice(shippingPriceOpt: Option[ShippingPrice]) : Long = {
+  def confirmShippingPrice(shippingPriceOpt: Option[ShippingPrice]): Long = {
     shippingPriceOpt.map { shippingPrice =>
-      val serviceOpt = servicesList.find { _.isManageShipmentId(shippingPrice) }
+      val serviceOpt = servicesList.find {
+        _.isManageShipmentId(shippingPrice)
+      }
       serviceOpt.map { service =>
         service.confirmShipmentId(shippingPrice)
       }.getOrElse(0L)
