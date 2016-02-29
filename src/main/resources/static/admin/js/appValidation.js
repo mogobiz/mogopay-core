32055490b369ce47f01340b87069f720cb36f87b
Ahmad Ibrahim <ahmad.ibrahim@technoagile.com>
2016-02-29 16:26:41 +0200

    JAHIAC-540

--- src/main/resources/static/admin/js/appValidation.js
+++ src/main/resources/static/admin/js/appValidation.js
@@ -40,3 +40,18 @@ mogopay.directive('ngFileChange', function() {
 		}
 	};
 });
+
+mogopay.filter("priceFilter", [
+function() { // should be altered to suit your needs
+    return function(input) {
+    input = parseFloat(input);
+    if(input % 1 === 0) {
+      input = input.toFixed(0);
+    }
+    else {
+      input = input.toFixed(2);
+    }
+
+    return input.toString();
+  };
+}]);
